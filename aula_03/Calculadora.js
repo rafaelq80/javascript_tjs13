59f808a6003114f2efffda12917ca17326d9c2c9
Rafael Queiroz <rafaelproinfo@gmail.com>
2026-01-28 11:38:39 -0300

    Live Code 03 - Exemplos - Estruturas Condicionais

---
+++ aula_03/Calculadora.js
@@ -0,0 +1,38 @@
+const leia = require("readline-sync");
+
+console.log("============================================");
+console.log("       CALCULADORA COM SWITCH CASE          ");
+console.log("============================================");
+console.log("(+) - Soma                                    ");
+console.log("(-) - Subtração                               ");
+console.log("(x) - Multiplicação                           ");
+console.log("(/) - Divisão                                 ");
+console.log("============================================");
+console.log("                                            ");
+
+let numero1 = leia.questionFloat("Digite o primeiro numero: ", 
+                                {limitMessage: 'Digite um numero float'}
+                                );
+
+let numero2 = leia.questionFloat("Digite o segundo numero: ", 
+                                {limitMessage: 'Digite um numero float'}
+                                );
+
+let operacao = leia.keyIn("Digite a operacao: ");
+
+switch(operacao){
+    case "+":
+        console.log(`${numero1} + ${numero2} = ${numero1 + numero2}`);
+    break;
+    case "-":
+        console.log(`${numero1} - ${numero2} = ${numero1 - numero2}`);
+    break;
+    case "x":
+        console.log(`${numero1} x ${numero2} = ${numero1 * numero2}`);
+    break;
+    case "/":
+        console.log(`${numero1} / ${numero2} = ${numero1 / numero2}`);
+    break;
+    default:
+        console.log("Operacao Invalida!");
+}
---
+++ aula_03/Ternario.js
@@ -0,0 +1,3 @@
+let idade = 21;
+
+idade >= 18 ? console.log("Maior de idade") : console.log("Menor de idade");
